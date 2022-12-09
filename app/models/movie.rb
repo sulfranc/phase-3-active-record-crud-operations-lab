d65df42398c37cee696498e7d5a5dd503f399f39
sulfranc <sulfranc@gmail.com>
2022-12-08 23:11:55 -0500

    commit tree 6c9cc1e8a6fb1c9fb1b8027ff261b0b688f7c8b8

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,47 @@
 class Movie < ActiveRecord::Base
 
+    def Movie.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    def Movie.first_movie
+        Movie.first
+    end
+    def Movie.last_movie
+        Movie.last
+    end
+
+    def Movie.movie_count
+        Movie.count
+    end
+
+    def Movie.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def Movie.find_movie_with_attributes(attribute)
+        Movie.find_by(attribute)
+    end
+
+    def Movie.find_movies_after_2002
+        Movie.where("release_date > 2002")
+    end
+
+    def update_with_attributes(attribute)#update_with_attributes
+        self.update(attribute)
+    end
+
+    def Movie.update_all_titles(title)
+        Movie.update(title: title)
+    end
+
+    def Movie.delete_by_id(id)
+        Movie.destroy(id)
+    end
+
+    def Movie.delete_all_movies
+        Movie.destroy_all
+    end
+
+
 end
